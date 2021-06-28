9e9248830f3a6d153ffbba6db37e789778c580c7
Sebastián Rodríguez <srodriguez81@gmail.com>
2021-06-28 17:02:45 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// selectores
+let mouseCursor = document.querySelector('.cursor')
+let navLinks = document.querySelectorAll('.nav-links li')
+
+// event listeners
+window.addEventListener('mousemove', cursor)
+
+// funciones
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + 'px'
+    mouseCursor.style.left = e.pageX + 'px'
+}
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('link-grow')
+        link.classList.remove('hovered-link')
+    })
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('link-grow')
+        link.classList.add('hovered-link')
+    })
+})
